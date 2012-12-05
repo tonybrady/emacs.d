aaf3d93843e6a799ea62ceee2a8bacffceba6378
Jon Ramsey <jonathon.ramsey@gmail.com>
2012-12-05 15:56:23 +0000

    init.el

---
+++ init.el
@@ -0,0 +1,27 @@
+(setq-default
+ user-full-name "Jonathon Ramsey"
+ user-mail-address "jonathon.ramsey@gmail.com"
+ mail-default-reply-to "jonathon.ramsey@gmail.com")
+
+(setq dotfiles-dir (file-name-directory
+		    (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "lib"))
+
+(setq custom-file (concat dotfiles-dir "custom.el"))
+(setq recentf-save-file (concat dotfiles-dir "recentf"))
+
+;; (require 'jon-misc)
+;; (require 'jon-osx)
+
+;; (require 'jon-packages)
+;; (require 'jon-starter-kit)
+;; (require 'jon-defuns)
+;; (require 'jon-bindings)
+;; (require 'jon-templates)
+;; (require 'jon-desktop)
+;; (require 'jon-startup)
+
+;; (setq custom-theme-directory "~/.emacs.d/themes")
+;; (load-theme 'tomorrow-night-modeline)
+(set-cursor-color "white")
