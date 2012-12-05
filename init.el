8950715809742d49085caf80f21aebee4c2be0dc
Jon Ramsey <jonathon.ramsey@gmail.com>
2012-12-05 16:07:51 +0000

    el-get

--- init.el
+++ init.el
@@ -14,6 +14,15 @@
 (require 'jon-misc)
 (require 'jon-osx)
 
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(el-get 'sync)
+
 ;; (require 'jon-packages)
 ;; (require 'jon-starter-kit)
 ;; (require 'jon-defuns)
