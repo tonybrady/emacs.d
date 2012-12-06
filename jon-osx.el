3cc703f923c9cdce5ca4af3d765746c8eaf381af
Jon Ramsey <jonathon.ramsey@gmail.com>
2012-12-06 14:36:35 +0000

    Simplifying paths.

--- jon-osx.el
+++ jon-osx.el
@@ -1,22 +1,10 @@
 (when (eq system-type 'darwin)
-  (setenv "PATH" (concat "/usr/local/mysql/bin:/usr/local/git/bin:/usr/local/bin:/usr/local/texlive/2012basic/bin/universal-darwin:"
-                         (getenv "PATH")
-                         ":/Users/jon/bin"))
+  (setenv "PATH" (concat (getenv "PATH") ":/Users/jon/bin"))
+  (add-to-list 'exec-path "/Users/jon/bin")
+
   (setenv "LANG" "en_GB.UTF-8")
   (setenv "LC_ALL" "en_GB.UTF-8")
   (setenv "LC_CTYPE" "en_GB.UTF-8")
-
-  (setq exec-path '("/usr/local/bin"
-                    "/usr/local/sbin"
-                    "/usr/local/mysql/bin"
-                    "/usr/bin"
-                    "/bin"
-                    "/usr/sbin"
-                    "/sbin"
-                    "/usr/X11/bin"
-                    "/usr/local/git/bin"
-                    "/usr/local/texlive/2012basic/bin/universal-darwin"
-                    "/Users/jon/bin"))
 
   (defun mac-run-ruby ()
     (interactive)
